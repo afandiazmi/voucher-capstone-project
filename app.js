58b9ae26efc64777ffddd65bf1a5061f810c8c4b
Muhammad Afandi <afandi_army@live.com>
2025-03-10 05:50:25 +0800

    dsreg56

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-const cookieParser = require('cookie-parser'); // Add this line
+const cookieParser = require('cookie-parser'); // Passing cookie to the browser
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
